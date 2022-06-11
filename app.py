4d4557066b63d1edc1119e6022c2dcdc8d3a89c4
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2022-06-11 17:30:23 -0500

    Added config.json and now works for multiple imports.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from functools import total_ordering
+import json
 from unittest import result
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -9,46 +10,53 @@ scope = 'playlist-modify-private playlist-read-private'
 
 spotify = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
-playlistFromId = ""
-playlistFromName = os.getenv('SPOTIPY_PLAYLIST_TO_EXPORT')
-playlistToId = ""
-playlistToName = os.getenv('SPOTIPY_PLAYLIST_TO_IMPORT') + str(date.today().year) if os.getenv('SPOTIPY_PLAYLIST_TO_IMPORT_ADD_YEAR') else ''
-limit = int(os.getenv('SPOTIPY_PAGE_LIMIT')) if os.getenv('SPOTIPY_PAGE_LIMIT') else 100
-offset = -limit
+limit = int(os.getenv('SPOTIPY_PAGE_LIMIT')) if os.getenv('SPOTIPY_PAGE_LIMIT') else 50
 
-while not playlistFromId and not playlistToId:
-    offset += limit
-    results = spotify.current_user_playlists(limit=limit, offset=offset)
-    # Find playlist and search all playlists for the playlist name
-    for playlist in results['items']:
-        if(playlist['owner']['id'] == 'spotify' and playlist['name'] == playlistFromName):
-            print(playlist['id'])
-            playlistFromId = playlist['id']
-        if(playlist['name'] == playlistToName):
-            print(playlist['id'])
-            playlistToId = playlist['id']
+config = json.load(open('config.json'))
+copyTracks = config["copyTracks"]
 
-if(playlistFromId):
-    if(not playlistToId):
-        playlistToId = spotify.user_playlist_create(spotify.me()['id'], playlistToName, public=False)['id']
-    if(playlistFromId and playlistToId):
-        # Get all tracks from playlist
-        results = spotify.playlist_tracks(playlist_id=playlistFromId, fields='items.track(id, name)')
-        fromTracks = results['items']
-        fromTrackIds = list(map(lambda x: x['track']['id'], fromTracks))
+def CopyTracks(playlistFromName, playlistToName, addYeartoPlaylistToName):
+    playlistToName += str(date.today().year) if addYeartoPlaylistToName else ''
+    offset = -limit
+    playlistFromId = ""
+    playlistToId = ""
 
-        offset = -limit
-        toTracks = []
-        while True:
-            offset += limit
-            results = spotify.playlist_tracks(playlist_id=playlistToId, fields='items.track(id, name)', limit=limit, offset=offset)
-            if(len(results['items']) == 0):
-                break
+    while not playlistFromId and not playlistToId:
+        offset += limit
+        results = spotify.current_user_playlists(limit=limit, offset=offset)
+        # Find playlist and search all playlists for the playlist name
+        for playlist in results['items']:
+            if(playlist['name'] == playlistFromName):
+                print(playlist['id'])
+                playlistFromId = playlist['id']
+            if(playlist['name'] == playlistToName):
+                print(playlist['id'])
+                playlistToId = playlist['id']
 
-            if(toTracks): toTracks.extend(results['items'])
-            else: toTracks = results['items']
+    if(playlistFromId):
+        if(not playlistToId):
+            playlistToId = spotify.user_playlist_create(spotify.me()['id'], playlistToName, public=False)['id']
+        if(playlistFromId and playlistToId):
+            # Get all tracks from playlist
+            results = spotify.playlist_tracks(playlist_id=playlistFromId, fields='items.track(id, name)')
+            fromTracks = results['items']
+            fromTrackIds = list(map(lambda x: x['track']['id'], fromTracks))
 
-        toTrackIds = list(map(lambda x: x['track']['id'], toTracks))
-        uniqueTrackIds = set(set(fromTrackIds) - set(toTrackIds))
-        # Add tracks to playlist
-        if(len(uniqueTrackIds) > 0): spotify.playlist_add_items(playlist_id=playlistToId, items=uniqueTrackIds)
+            offset = -limit
+            toTracks = []
+            while True:
+                offset += limit
+                results = spotify.playlist_tracks(playlist_id=playlistToId, fields='items.track(id, name)', limit=limit, offset=offset)
+                if(len(results['items']) == 0):
+                    break
+
+                if(toTracks): toTracks.extend(results['items'])
+                else: toTracks = results['items']
+
+            toTrackIds = list(map(lambda x: x['track']['id'], toTracks))
+            uniqueTrackIds = set(set(fromTrackIds) - set(toTrackIds))
+            # Add tracks to playlist
+            if(len(uniqueTrackIds) > 0): spotify.playlist_add_items(playlist_id=playlistToId, items=uniqueTrackIds)
+
+for copyTrack in copyTracks:
+    CopyTracks(copyTrack["playlistFromName"], copyTrack["playlistToName"], copyTrack["addCurrentYearToPlaylistToName"])
